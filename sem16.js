10101ad9bc475eae3743b6af42f7fb9412c04860
Dinely Pinco Reyes <dinely@rapihost.com>
2022-11-17 19:49:48 -0500

    semana16

---
+++ sem16.js
@@ -0,0 +1,19 @@
+//Ejercicio1
+function restarnumero(numero1,numero2){
+    let resultado = numero1 - numero2
+    alert(resultado)
+    
+}
+//Ejercicio2
+function grados(){
+    let gradosfarenheit = +prompt("Ingrese grados");
+    let resultado = (gradosfarenheit- 32)*(5/9);
+    alert(resultado)
+}
+//Ejercicio3
+function sumarnumeros(){
+    let numero1 = +prompt("Ingrese Nunero 1");
+    let numero2 = +prompt("Ingrese Numero 2");
+    let resultado = numero1 + numero2
+    alert(resultado)
+}
